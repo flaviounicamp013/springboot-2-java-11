95314b348f15c93ae934ddcda2625693c20c5483
Flavio Cosenza <flaviounicamp013@gmail.com>
2020-07-27 19:13:30 -0300

    Category

--- src/main/java/com/github/demo/config/TestConfig.java
+++ src/main/java/com/github/demo/config/TestConfig.java
@@ -8,9 +8,11 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
+import com.github.demo.entities.Category;
 import com.github.demo.entities.Order;
 import com.github.demo.entities.User;
 import com.github.demo.entities.enums.OrderStatus;
+import com.github.demo.repositories.CategoryRepository;
 import com.github.demo.repositories.OrderRepository;
 import com.github.demo.repositories.UserRepository;
 
@@ -23,9 +25,18 @@ public class TestConfig implements CommandLineRunner {
 	
 	@Autowired
 	private OrderRepository orderRepository;
+	
+	@Autowired
+	private CategoryRepository categoryRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
+		
+		Category cat1 = new Category(null, "Electronics");
+		Category cat2 = new Category(null, "Books");
+		Category cat3 = new Category(null, "Computers"); 
+		
+		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
 
 		User u1 = new User(null, "Maria Brown", "maria@gmail.com", "988888888", "123456");
 		User u2 = new User(null, "Alex Green", "alex@gmail.com", "977777777", "123456"); 
