7b4ab68a8036bbf2ca01de02be707ce0496b412f
Flavio Cosenza <flaviounicamp013@gmail.com>
2020-09-12 16:28:08 -0300

    Payment, One-to-one Association

--- src/main/java/com/github/demo/config/TestConfig.java
+++ src/main/java/com/github/demo/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.github.demo.entities.Category;
 import com.github.demo.entities.Order;
 import com.github.demo.entities.OrderItem;
+import com.github.demo.entities.Payment;
 import com.github.demo.entities.Product;
 import com.github.demo.entities.User;
 import com.github.demo.entities.enums.OrderStatus;
@@ -81,5 +82,9 @@ public class TestConfig implements CommandLineRunner {
 			
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 }
