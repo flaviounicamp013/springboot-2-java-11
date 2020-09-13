7750441778824519199813e2e8e4500fb6af7036
Flavio Cosenza <flaviounicamp013@gmail.com>
2020-09-13 16:15:18 -0300

    Exception Handling - Delete

--- src/main/java/com/github/demo/services/UserService.java
+++ src/main/java/com/github/demo/services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.github.demo.entities.User;
 import com.github.demo.repositories.UserRepository;
+import com.github.demo.services.exceptions.DatabaseException;
 import com.github.demo.services.exceptions.ResourceNotFoundException;
 
 
@@ -32,7 +35,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		} catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		} catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
